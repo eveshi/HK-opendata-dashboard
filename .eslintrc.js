33c4183e21a04b0c49b319fe5721aea1edb168fb
eve shi <play.sos@gmail.com>
2020-10-15 16:19:01 -0400

    init setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: ["react"], // use the plugin rules within ESLint
+  parser: ["babel-eslint"],
+  extends: [
+    "prettier",
+    "prettier/babel",
+    "prettier/react"
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      pragma: "React", // Pragma to use, default to "React"
+      version: "detect" // React version
+    }
+  }
+};
